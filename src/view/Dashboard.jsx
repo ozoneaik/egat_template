f8f130f98fab0429c1f83de0ae448581bdf5a165
Phuwadech Panichayasopa <aofphuwadech@gmail.com>
2024-01-07 05:53:41 +0700

    create content on Dashboard

--- src/view/Dashboard.jsx
+++ src/view/Dashboard.jsx
@@ -2,6 +2,15 @@ import Layout from "../layouts/Layout.jsx";
 
 export default function Dashboard(){
     return (
-         <Layout>this is Dashboard page</Layout>
+         <Layout>
+             <h2 className="fs-5">Dashboard</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, totam? Sequi alias eveniet
+                 ut
+                 quas
+                 ullam delectus et quasi incidunt rem deserunt asperiores reiciendis assumenda doloremque
+                 provident,
+                 dolores aspernatur neque.
+             </p>
+         </Layout>
     )
 }
